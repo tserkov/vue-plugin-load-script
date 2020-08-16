1823e25c15f9db82be409d0733fe798e9edd27ba
Bjornar97 <hetlest@hotmail.com>
2020-08-16 23:00:10 +0200

    Changed double quotes back to single quotes

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ const LoadScript = {
         let shouldAppend = false;
         let el = document.querySelector('script[src="' + src + '"]');
         if (!el) {
-          el = document.createElement("script");
-          el.type = "text/javascript";
+          el = document.createElement('script');
+          el.type = 'text/javascript';
           el.async = true;
           el.src = src;
           shouldAppend = true;
-        } else if (el.hasAttribute("data-loaded")) {
+        } else if (el.hasAttribute('data-loaded')) {
           resolve(el);
           return;
         }
 
-        el.addEventListener("error", reject);
-        el.addEventListener("abort", reject);
-        el.addEventListener("load", function loadScriptHandler() {
-          el.setAttribute("data-loaded", true);
+        el.addEventListener('error', reject);
+        el.addEventListener('abort', reject);
+        el.addEventListener('load', function loadScriptHandler() {
+          el.setAttribute('data-loaded', true);
           resolve(el);
         });
 
