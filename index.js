cbecc16acbbe612d46a2d481cab55c944bc8a2c4
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2021-04-14 00:11:51 +0300

    Revert "Multiple loading with Array has been added"
    This reverts commit e230217ca70d24edc5807e9227405114718df5e7.

--- index.js
+++ index.js
@@ -1,94 +1,47 @@
 const LoadScript = {
   install: function (Vue) {
-    Vue.loadScript = Vue.prototype.$loadScript = (source, root = "head") => {
-      return new Promise((resolve, reject) => {
-        let el;
+    Vue.loadScript = Vue.prototype.$loadScript = function (src) { // eslint-disable-line no-param-reassign
+      return new Promise(function (resolve, reject) {
         let shouldAppend = false;
-
-        if (typeof source === "object") {
-          source.forEach((src) => {
-            el = checkScript(src);
-
-            if (!el) {
-              el = createScript(src);
-              shouldAppend = true;
-            } else if (el.hasAttribute("data-loaded")) {
-              resolve(el);
-              return;
-            }
-
-            el.addEventListener("error", reject);
-            el.addEventListener("abort", reject);
-            el.addEventListener("load", function loadScriptHandler() {
-              el.setAttribute("data-loaded", true);
-              resolve(el);
-            });
-
-            if (shouldAppend)
-              document.getElementsByTagName(root)[0].appendChild(el);
-          });
-        } else if (typeof source === "string") {
-          el = checkScript(source);
-
-          if (!el) {
-            el = createScript(source);
-            shouldAppend = true;
-          } else if (el.hasAttribute("data-loaded")) {
-            resolve(el);
-            return;
-          }
-
-          if (shouldAppend)
-            document.getElementsByTagName(root)[0].appendChild(el);
+        let el = document.querySelector('script[src="' + src + '"]');
+        if (!el) {
+          el = document.createElement('script');
+          el.type = 'text/javascript';
+          el.async = true;
+          el.src = src;
+          shouldAppend = true;
         }
+        else if (el.hasAttribute('data-loaded')) {
+          resolve(el);
+          return;
+        }
+
+        el.addEventListener('error', reject);
+        el.addEventListener('abort', reject);
+        el.addEventListener('load', function loadScriptHandler() {
+          el.setAttribute('data-loaded', true);
+          resolve(el);
+        });
+
+        if (shouldAppend) document.head.appendChild(el);
       });
     };
 
-    Vue.unloadScript = Vue.prototype.$unloadScript = (source, root) => {
-      return new Promise((resolve, reject) => {
-        let el;
+    Vue.unloadScript = Vue.prototype.$unloadScript = function (src) { // eslint-disable-line no-param-reassign
+      return new Promise(function (resolve, reject) {
+        const el = document.querySelector('script[src="' + src + '"]');
 
-        if (typeof source === "object") {
-          source.forEach((src) => {
-            el = checkScript(src);
-
-            if (!el) {
-              reject();
-              return;
-            }
-
-            document.getElementsByTagName(root)[0].removeChild(el);
-
-            resolve();
-          });
-        } else if (typeof source === "string") {
-          el = checkScript(source);
-
-          if (!el) {
-            reject();
-            return;
-          }
-
-          document.getElementsByTagName(root)[0].removeChild(el);
-
-          resolve();
+        if (!el) {
+          reject();
+          return;
         }
+
+        document.head.removeChild(el);
+
+        resolve();
       });
     };
   },
-};
-
-const checkScript = (src) => {
-  return document.querySelector('script[src="' + src + '"]');
-};
-
-const createScript = (src) => {
-  let el = document.createElement("script");
-  el.type = "text/javascript";
-  el.async = true;
-  el.src = src;
-
-  return el;
 };
 
 export default LoadScript;
