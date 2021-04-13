79aa02d37c9c928666068ee16a30e834b19d9138
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2021-04-14 02:16:27 +0300

    added missing resolve() functions

--- index.js
+++ index.js
@@ -1,6 +1,55 @@
+// const LoadScript = {
+//   install: function (Vue) {
+//     Vue.loadScript = Vue.prototype.$loadScript = function (src) { // eslint-disable-line no-param-reassign
+//       return new Promise(function (resolve, reject) {
+//         let shouldAppend = false;
+//         let el = document.querySelector('script[src="' + src + '"]');
+//         if (!el) {
+//           el = document.createElement('script');
+//           el.type = 'text/javascript';
+//           el.async = true;
+//           el.src = src;
+//           shouldAppend = true;
+//         }
+//         else if (el.hasAttribute('data-loaded')) {
+//           resolve(el);
+//           return;
+//         }
+
+//         el.addEventListener('error', reject);
+//         el.addEventListener('abort', reject);
+//         el.addEventListener('load', function loadScriptHandler() {
+//           el.setAttribute('data-loaded', true);
+//           resolve(el);
+//         });
+
+//         if (shouldAppend) document.head.appendChild(el);
+//       });
+//     };
+
+//     Vue.unloadScript = Vue.prototype.$unloadScript = function (src) { // eslint-disable-line no-param-reassign
+//       return new Promise(function (resolve, reject) {
+//         const el = document.querySelector('script[src="' + src + '"]');
+
+//         if (!el) {
+//           reject();
+//           return;
+//         }
+
+//         document.head.removeChild(el);
+
+//         resolve();
+//       });
+//     };
+//   },
+// };
+
+// export default LoadScript;
+
+
 const LoadScript = {
   install: function(Vue) {
-    Vue.loadScript = Vue.prototype.$loadScript = (source, root = "head") => {
+    Vue.loadScript = Vue.prototype.$loadScript = (source, root = "head") => { // eslint-disable-line no-param-reassign
       return new Promise((resolve, reject) => {
         let el;
         let shouldAppend = false;
@@ -26,6 +75,8 @@ const LoadScript = {
 
             if (shouldAppend)
               document.getElementsByTagName(root)[0].appendChild(el);
+            
+            resolve();
           });
         } else if (typeof source === "string") {
           el = checkScript(source);
@@ -37,14 +88,16 @@ const LoadScript = {
             resolve(el);
             return;
           }
-
+          
           if (shouldAppend)
             document.getElementsByTagName(root)[0].appendChild(el);
+          
+          resolve();
         }
       });
     };
 
-    Vue.unloadScript = Vue.prototype.$unloadScript = (source, root) => {
+    Vue.unloadScript = Vue.prototype.$unloadScript = (source, root) => { // eslint-disable-line no-param-reassign
       return new Promise((resolve, reject) => {
         let el;
 
