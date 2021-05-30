ec5268aa4806319e279da829c89f451f3d25aa83
Bjornar97 <hetlest@hotmail.com>
2021-05-30 18:28:30 +0200

    Fixed indentation and renamed "thisLoadScript" to "plugin"

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 function loadScript(src) {
-      // eslint-disable-line no-param-reassign
-      return new Promise(function (resolve, reject) {
-        let shouldAppend = false;
-        let el = document.querySelector('script[src="' + src + '"]');
-        if (!el) {
-          el = document.createElement('script');
-          el.type = 'text/javascript';
-          el.async = true;
-          el.src = src;
-          shouldAppend = true;
-        } else if (el.hasAttribute('data-loaded')) {
-          resolve(el);
-          return;
-        }
+  // eslint-disable-line no-param-reassign
+  return new Promise(function (resolve, reject) {
+    let shouldAppend = false;
+    let el = document.querySelector('script[src="' + src + '"]');
+    if (!el) {
+      el = document.createElement("script");
+      el.type = "text/javascript";
+      el.async = true;
+      el.src = src;
+      shouldAppend = true;
+    } else if (el.hasAttribute("data-loaded")) {
+      resolve(el);
+      return;
+    }
 
-        el.addEventListener('error', reject);
-        el.addEventListener('abort', reject);
-        el.addEventListener('load', function loadScriptHandler() {
-          el.setAttribute('data-loaded', true);
-          resolve(el);
-        });
+    el.addEventListener("error", reject);
+    el.addEventListener("abort", reject);
+    el.addEventListener("load", function loadScriptHandler() {
+      el.setAttribute("data-loaded", true);
+      resolve(el);
+    });
 
-        if (shouldAppend) document.head.appendChild(el);
-      });
-    };
+    if (shouldAppend) document.head.appendChild(el);
+  });
+}
 
 function unloadScript(src) {
   // eslint-disable-line no-param-reassign
@@ -43,11 +43,11 @@ function unloadScript(src) {
 
 export { unloadScript, loadScript };
 
-const thisLoadScript = {
+const plugin = {
   install: function (app) {
     app.config.globalProperties.$loadScript = loadScript;
     app.config.globalProperties.$unloadScript = unloadScript;
   },
 };
 
-export default thisLoadScript;
+export default plugin;
