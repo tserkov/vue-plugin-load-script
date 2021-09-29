f7836aa49a4814ab04f3bae29a4877558d03c5b3
tserkov <3517234+tserkov@users.noreply.github.com>
2021-09-29 21:58:36 +0100

    fix: add typescript typings

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+declare module "vue-plugin-load-script";
+
+declare module "vue/types/vue" {
+  interface Vue {
+    $loadScript: (src: string) => Promise<HTMLScriptElement>;
+    $unloadScript: (src: string) => Promise<void>;
+  }
+}
